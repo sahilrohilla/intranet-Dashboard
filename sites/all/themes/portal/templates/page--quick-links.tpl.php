62c691d43ff530034b7b1388e6ad77524ee1005c
Sahil Rohilla <sahil.rohilla@hogarthww.com>
2016-04-29 19:10:19 +0530

    latest push

---
+++ sites/all/themes/portal/templates/page--quick-links.tpl.php
@@ -0,0 +1,15 @@
+<style>
+#admin-menu{
+  display:none;
+}
+#outerImageContainer {
+    background-color: transparent;
+    overflow: initial;
+}
+#imageDataContainer{
+    display: none;
+    }
+</style>
+<?php
+print views_embed_view('quick_links', 'block_1');
+?>
